1596d3ff246d416ad234147bcbb62610702638b8
Tigrino22 <valentin.legris@outlook.com>
2024-06-20 16:06:13 +0200

    gestion de l'affichage des réservations

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,12 +5,19 @@ import {Route} from "./Route.js";
  * @type {Route[]}
  */
 export const allRoutes = [
+// APP
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "Galerie", "/pages/galerie.html"),
+
+// AUTH
     new Route("/login", "Connexion", "/pages/auth/login.html"),
     new Route("/register", "Inscription", "/pages/auth/register.html"),
     new Route("/account", "Mon Compte", "/pages/auth/account.html"),
     new Route("/edit-password", "Edit Password", "/pages/auth/editPassword.html"),
+
+// RESERVATIONS
+    new Route("/allresa", "Vos réservations", "/pages/reservations/allresa.html"),
+    
 ]
 
 // Affichera Route.titre - nameWebsite
